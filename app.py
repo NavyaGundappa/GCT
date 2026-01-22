5b1d000dfb7174086f7efafc3cd114fd589dffa5
navyagundappa92 <navyagundappa92@gmail.com>
2026-01-22 14:18:07 +0530

    updated frontend

--- app.py
+++ app.py
@@ -5,8 +5,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('index2.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index1.html')
+
+
+@app.route('/index2')
+def index2():
     return render_template('index.html')
 
 
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+
+@app.route('/index4')
+def index4():
+    return render_template('index4.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(host="0.0.0.0", port=5000, debug=True)
